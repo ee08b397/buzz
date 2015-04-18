31d27e400ca44117ad2d3a6368f7551c80251497
Jeff Revesz <jeff.revesz@buzzfeed.com>
2015-04-18 19:38:52 -0400

    cleaned up parser

--- parser/json2graph.py
+++ parser/json2graph.py
@@ -1,173 +1,79 @@
 import json, os, sys
 from pprint import pprint
 
-data = json.loads(open("edges.json").read())
-
-# pprint(data)
-# print json.dumps(data,indent=1)
-
+raw = json.loads(open("edges.json").read())
+edges = [{'p': d['parent'], 'c': d['child']} for d in raw]
 
 # get # of links
-print(len(data))
+print "{0} edges in raw data".format(len(edges))
 
-'''
-# get # of nodes
-dic = {}
-# for i in range(5):
-for i in range(len(data)):
-    dic[data[i]["parent"]] = 1
-    dic[data[i]["child"]] = 1
-print(len(dic))
+# remove edges with duplicate children: cleans parallel edges and circles
+edges = {e['c']:e for e in edges}.values()
 
-'''
+print "{0} edges with duplicate children removed".format(len(edges))
 
-dupcount = []
-#for i in range(10):
-#    for j in xrange(i + 1,10):
-for i in range(len(data)):
-    for j in xrange(i + 1, len(data)):
-        #if data[i]["child"] == data[j]["child"] and data[i]["parent"] != data[j]["parent"]:
-        if data[i]["child"] == data[j]["child"]:
-            print (
-                    str(data[i]["parent"]) + " " + str(data[i]["child"]) + 
-                    ") and (" + 
-                    str(data[j]["parent"]) + " " + str(data[j]["child"])
-                  )
-            dupcount.append(j)
-
-dupcount.sort()
-print len(dupcount)
-for i in range(len(dupcount)):
-    data[dupcount[i] - i]
-    del data[dupcount[i] - i]
-
-# for i in xrange(50, len(data)):
-#    del data[50]
-
-print(len(data))
-
-
-# unique set of parent and child ids
-parent_ids = set([d["parent"] for d in data]) 
-child_ids = set([d["child"] for d in data]) 
-
-# all ids
+# get unique sets of ids
+parent_ids = set([e['p'] for e in edges]) 
+child_ids = set([e['c'] for e in edges]) 
 all_ids = parent_ids.union(child_ids) 
 
-# root nodes only
+# get disconnected root nodes
 root_node_ids = parent_ids - child_ids
 
-print "no of free roots: ", str(len(root_node_ids))
+print "found {0} disconnected root nodes".format(len(root_node_ids))
 
-# ancestor node
+# create ancestor node
 ancestor_id = max(all_ids) + 1
 
-# ancestor edges
-for root_node_id in root_node_ids:
-    data.append({'parent': ancestor_id,
-                 'child': root_node_id})
-    print "added ancestor edge: {0}, {1}".format(ancestor_id, root_node_id)
+print "created ancestor_id: {0}".format(ancestor_id)
 
-# make sure all circles cleaned
-dupcount = []
-#for i in range(10):
-#    for j in xrange(i + 1,10):
-for i in range(len(data)):
-    for j in xrange(i + 1, len(data)):
-        if data[i]["child"] == data[j]["child"] and data[i]["parent"] != data[j]["parent"]:
-            print (
-                    str(data[i]["parent"]) + " " + str(data[i]["child"]) + 
-                    ") and (" + 
-                    str(data[j]["parent"]) + " " + str(data[j]["child"])
-                  )
-            dupcount.append(j)
+# add ancestor edges
+ancestor_edges = [{'p': ancestor_id,
+		   'c': root_node_id} for root_node_id in root_node_ids]
 
-print dupcount
+print "created {0} ancestor edges".format(len(ancestor_edges))
 
-# get # of nodes
-dic = {}
-# for i in range(5):
-for i in range(len(data)):
-    dic[data[i]["parent"]] = 1
-    dic[data[i]["child"]] = 1
-print(len(dic))
+edges.extend(ancestor_edges)
 
+print "{0} edges in final tree".format(len(edges))
 
+# sort edges by parent id:
+edges = sorted(edges, key=lambda e: e['p'])
 
+edges_str =  ",\n\t\t".join(["{{ {0}; {1}; }}".format(edge['p'], edge['c']) for edge in edges])
+link_values_str =  ",\n\t\t\t\t".join(["{{ @id={0}; @value=T; }}".format(x) for x in xrange(0,len(edges) + 1)])
 
-
-out = "Graph\n{\n"
-# name
-out += "\t\"name\";\n"
-
-# description
-out += "\t\"desc\";\n"
-
-# #nodes, #links, #paths, and #path links
-# out += "\t" + str(len(dic)) + "; " + str(len(data)) + "; 0; 0;\n"  
-out += "\t" + str(ancestor_id+1) + "; " + str(len(data)) + "; 0; 0;\n"  
-# out += "\t" + str(7) + "; " + str(5) + "; 0; 0;\n"  
-
-out += "\t[\n"
-# for i in range(4):
-for i in range(len(data) - 1):
-    out += "\t\t{ " + str(data[i]["parent"]) + "; " + str(data[i]["child"]) + "; },\n"
-    # pprint(data[i]["parent"] + data[i]["child"])
-    # print(str(data[i]["parent"]), " ", str(data[i]["child"]))
-out += "\t\t{ " + str(data[len(data)-1]["parent"]) + "; " + str(data[len(data) - 1]["child"]) + "; }\n"
-out += "\t];\n"
-
-# path list
-out += "\t; \n" 
-
-
-out += ("\t@enumerations=;\n" + 
-       "\t@attributeDefinitions=[\n"+
-       "\t\t{\n"+
-       "\t\t\t@name=$root;\n"+
-       "\t\t\t@type=bool;\n"+
-       "\t\t\t@default=|| false ||;\n"+
-       "\t\t\t@nodeValues=[ { @id=0; @value=T; } ];\n"+
-       "\t\t\t@linkValues=;\n"+
-       "\t\t\t@pathValues=;\n"+
-       "\t\t},\n"+
-       "\t\t{\n"+
-       "\t\t\t@name=$tree_link;\n"+
-       "\t\t\t@type=bool;\n"+
-       "\t\t\t@default=|| false ||; \n"+
-       "\t\t\t@nodeValues=;\n"+
-       "\t\t\t@linkValues=[\n")
-
-# for i in range(4):
-# for i in range(len(dic) - 1):
-# for i in range(3133):
-for i in range(len(data) - 1):
-#for i in dic:
-    out += "\t\t\t\t{ @id=" + str(i) + "; @value=T; },\n"
-out += "\t\t\t\t{ @id=" + str(len(data) - 1) + "; @value=T; }\n"
-
-out += (" ];\n" + 
-    "@pathValues=; } ]; \n"+
-    "@qualifiers=[ { \n" + 
-    "@type=$spanning_tree; \n" + 
-    "@name=$sample_spanning_tree; \n" +
-    "@description=; \n" + 
-    "@attributes=[ { \n" + 
-    "@attribute=0; \n" + 
-    "@alias=$root; }, { \n" + 
-    "@attribute=1; \n" + 
-    "@alias=$tree_link; } ]; } \n" +
-    "];")
-
-# visualization hints
-out += "\t; ; ; ;\n"
-
-# interface hints
-out += "\t; ; ; ; ;\n"
-
-# file end
-out += "}\n"
-# print out
+out = """Graph
+{{
+	"name";
+	"desc";
+	{max_node_id}; {num_edges}; 0; 0;
+	[
+		{edges}
+	];
+	@enumerations=;
+	@attributeDefinitions=[
+		{{
+			@name=$root;
+			@type=bool;
+			@default=|| false ||;
+			@nodeValues=[ {{ @id=0; @value=T; }} ];
+			@linkValues=;
+			@pathValues=;
+		}},
+		{{
+			@name=$tree_link;
+			@type=bool;
+			@default=|| false ||;
+			@nodeValues=;
+			@linkValues=[
+				{link_values}
+			];
+			@pathValues=;
+		}};
+	];
+}}
+""".format(max_node_id=ancestor_id + 1, num_edges=len(edges) + 1, edges=edges_str, link_values=link_values_str)
 
 f = open('out.graph', 'w')
 f.write(out)
